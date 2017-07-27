22a3e60672770f04df61e8fe42c9f46249343263
eileen <emcnaughton@wikimedia.org>
2017-07-27 14:54:37 +1200

    Default to only downloading opted in contacts

--- CRM/Omnimail/Omnigroupmembers.php
+++ CRM/Omnimail/Omnigroupmembers.php
@@ -35,6 +35,10 @@ class CRM_Omnimail_Omnigroupmembers extends CRM_Omnimail_Omnimail{
     $settings = CRM_Omnimail_Helper::getSettings();
 
     $mailerCredentials = CRM_Omnimail_Helper::getCredentials($params);
+    $jobParameters = array();
+    if ($params['is_opt_in_only']) {
+      $jobParameters['exportType'] = 'OPT_IN';
+    }
 
     $request = Omnimail::create($params['mail_provider'], $mailerCredentials)->getGroupMembers();
 
